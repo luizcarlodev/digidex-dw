40ad6349c841741593a433ac0641c5372d047e17
Luiz1Carlos <medeiros.luiz1@escolar.ifrn.edu.br>
2026-08-28 07:14:19 -0300

    update

---
+++ classroom/27-08-2026/js/script.js
@@ -0,0 +1,6 @@
+console.log('Olá!')
+window.alert('Oii!')
+
+let galoId = document.getElementById('galo-id')
+galoId.textContent = "vai dar o anus julio brito"
+galoId.style.color = "rgb(43, 255, 0)"
